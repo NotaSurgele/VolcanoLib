c86b5871d9d57f3d1afe58d488eb47df5709c4f6
alexis.picard@medialight.com <alexis.picard@medialight.com>
2021-09-01 16:05:46 +0400

    add Player class

---
+++ core/src/com/mygdx/game/Player.java
@@ -0,0 +1,21 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.volcano.game.Players;
+
+public class Player extends Players {
+
+    public Player(Sprite sprite, float x, float y) {
+        super(sprite, x, y);
+    }
+
+    public Player(Sprite sprite, Vector2 position) {
+        super(sprite, position);
+    }
+
+    public void test()
+    {
+        System.out.println(this.position);
+    }
+}
